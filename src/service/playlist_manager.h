0330f76ff41387f0f75ddd7933ca8e310aa40dbf
ivanovandrey78 <jaydenpull1@gmail.com>
2026-09-01 11:14:04 +0300

    feat(service): add PlaylistManager constructor holding a reference to MediaBundle playlists

--- src/service/playlist_manager.h
+++ src/service/playlist_manager.h
@@ -2,14 +2,18 @@
 
 #include <string>
 
+#include <core/media_bundle.h>
+#include <core/playlist.h>
+
 namespace mlm {
 
 class PlaylistManager {
 public: 
-
+    PlaylistManager(MediaBundle::playlists_t& pls)
+        : playlists_reference_(pls) {}
 
 private:
-    
+    MediaBundle::playlists_t& playlists_reference_;
 };
     
 } // namespace mlm
